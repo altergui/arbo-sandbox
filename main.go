77211e9522a17be5c84185b728557a0b78963f68
Gui Iribarren <gui@altermundi.net>
2024-10-22 11:45:59 +0200

    build a bigger tree in go

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/gob"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -11,9 +15,112 @@ import (
 	"go.vocdoni.io/dvote/db"
 	"go.vocdoni.io/dvote/db/metadb"
 	"go.vocdoni.io/dvote/tree/arbo"
+	"go.vocdoni.io/dvote/types"
 )
 
+func RandomBytes(n int) []byte {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+type Results struct {
+	Votes  [][]*big.Int  `json:"votes"`
+	Weight *types.BigInt `json:"weight"`
+}
+
+func (r Results) Bytes() []byte {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	err := encoder.Encode(r)
+	if err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
+
 func main() {
+	dir, err := os.MkdirTemp("", "arbosandbox")
+	if err != nil {
+		panic(err)
+	}
+
+	database, err := metadb.New(db.TypePebble, dir)
+	if err != nil {
+		panic(err)
+	}
+
+	tree, err := arbo.NewTree(arbo.Config{
+		Database: database, MaxLevels: 256,
+		HashFunction: arbo.HashFunctionBlake3,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	processID := RandomBytes(32)
+	censusRoot := RandomBytes(32)
+	ballotMode := []byte("1234")
+	encryptionKey := RandomBytes(32)
+	resultsAdd := Results{Votes: [][]*big.Int{
+		{big.NewInt(10), big.NewInt(5)},
+	}}
+	resultsSub := Results{Votes: [][]*big.Int{
+		{big.NewInt(2), big.NewInt(0)},
+	}}
+
+	if err := tree.Add([]byte{0x00}, processID); err != nil {
+		panic(err)
+	}
+	if err := tree.Add([]byte{0x01}, censusRoot); err != nil {
+		panic(err)
+	}
+	if err := tree.Add([]byte{0x02}, ballotMode); err != nil {
+		panic(err)
+	}
+	if err := tree.Add([]byte{0x03}, encryptionKey); err != nil {
+		panic(err)
+	}
+	if err := tree.Add([]byte{0x04}, resultsAdd.Bytes()); err != nil {
+		panic(err)
+	}
+	if err := tree.Add([]byte{0x05}, resultsSub.Bytes()); err != nil {
+		panic(err)
+	}
+
+	n := 200
+	v := RandomBytes(32)
+	fmt.Printf("value=%x\n", v)
+	for i := 1; i <= n; i++ {
+		k := binary.BigEndian.AppendUint64(nil, uint64(i))
+		if i%(n/10) == 0 {
+			fmt.Printf("adding leaves... i=%d, k=%x, v=%x\n", i, k, v)
+		}
+		if err := tree.Add(k, v); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(tree.Root())
+
+	cvp, err := tree.GenerateCircomVerifierProof(binary.BigEndian.AppendUint64(nil, uint64(n)))
+	if err != nil {
+		panic(err)
+	}
+
+	jCvp, err := json.Marshal(cvp)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := os.WriteFile("merkleproof.json", jCvp, os.ModePerm); err != nil {
+		panic(err)
+	}
+}
+
+func TestMockProof() {
 	dir, err := os.MkdirTemp("", "arbosandbox")
 	if err != nil {
 		panic(err)
@@ -40,8 +147,8 @@ func main() {
 	}
 	bLen := 1
 	for i := 0; i < len(testVector); i++ {
-		k := arbo.BigIntToBytes(bLen, big.NewInt(testVector[i][0]))
-		v := arbo.BigIntToBytes(bLen, big.NewInt(testVector[i][1]))
+		k := arbo.BigIntToBytesLE(bLen, big.NewInt(testVector[i][0]))
+		v := arbo.BigIntToBytesLE(bLen, big.NewInt(testVector[i][1]))
 		fmt.Println("Xxxxxx")
 
 		if err := tree.Add(k, v); err != nil {
@@ -53,7 +160,7 @@ func main() {
 
 	fmt.Println("Xxxxxx")
 	// proof of existence (fnc = 0: inclusion, 1: non inclusion)
-	k := arbo.BigIntToBytes(bLen, big.NewInt(int64(2)))
+	k := arbo.BigIntToBytesLE(bLen, big.NewInt(int64(2)))
 	cvp, err := tree.GenerateCircomVerifierProof(k)
 	if err != nil {
 		panic(err)
@@ -102,7 +209,7 @@ func main() {
 
 	fmt.Println(hex.EncodeToString(jCvp))
 	// proof of non-existence (fnc = 0: inclusion, 1: non inclusion)
-	k = arbo.BigIntToBytes(bLen, big.NewInt(int64(5)))
+	k = arbo.BigIntToBytesLE(bLen, big.NewInt(int64(5)))
 	cvp, err = tree.GenerateCircomVerifierProof(k)
 	if err != nil {
 		panic(err)
